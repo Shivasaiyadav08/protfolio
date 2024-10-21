2ff71f7e955602f4c4312c33f0c6dbb5eabfd376
Shivasaiyadav08 <shivasaiyadav08@gmail.com>
2024-10-21 16:06:53 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    screens:{
+      sm:"390px",
+      md:"670px",
+      lg:"768px",
+      xl:"1180px"
+    },
+    extend: {},
+    
+  },
+  plugins: [],
+}
+
+ 
